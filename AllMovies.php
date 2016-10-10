cc979e1eba9fde8d798f962105e5e8ac642333ab
Alexander Ladd <xander19@btinternet.com>
2016-10-10 15:45:17 +0100

    initial request

---
+++ AllMovies.php
@@ -0,0 +1,25 @@
+<?php
+//connection to csdm - webdev server and select DB
+$db = new mysqli(
+    'us-cdbr-azure-southcentral-f.cloudapp.net',
+    'b5051f6adfc120',
+    'c31439e7',
+    'webappalexander'
+);
+
+if($db->connect_errno){
+    die('Connection failed['.$db->connect_error.']');
+}
+
+$sql_query="SELECT * FROM marvelmovies WHERE title LIKE '%%'";
+
+$result = $db->query($sql_query);
+
+while($row = $result->fetch_array()){
+    echo"<p>" . $row['title'] . "</p>";
+}
+
+$result->close();
+$db->close();
+
+?>
