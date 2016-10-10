9c160cd3f4908764e884c6e96b3a80b96e60c007
Alexander Ladd <xander19@btinternet.com>
2016-10-10 14:44:32 +0100

    initial request

--- connectionDB.php
+++ connectionDB.php
@@ -1,12 +1,12 @@
 <?php
 // connect to csdm- webdev server and sel ect database
 $db = new mysqli(
-‘ ‘us-cdbr-azure-southcentral-f.cloudapp.net’ ’,
-‘ ‘b5051f6adfc120’ ’,
-‘ ‘c31439e7’ ’,
-‘ ‘webappalexander’ ’
+‘us-cdbr-azure-southcentral-f.cloudapp.net’,
+‘b5051f6adfc120’,
+‘c31439e7’,
+‘webappalexander’
 );
 // test if connection was established, and print any errors
-i f($db->connect _errno){
-    di e(‘Connectfailed['.$db->connect _error.']');
+if($db->connect_errno){
+    die(‘Connectfailed['.$db->connect _error.']');
 }
