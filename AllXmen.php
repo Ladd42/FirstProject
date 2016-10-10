6f8d76ae8768d9efc6f3ca09711c631fd56a0a31
Alexander Ladd <xander19@btinternet.com>
2016-10-10 15:57:30 +0100

    initial request

---
+++ AllXmen.php
@@ -0,0 +1,28 @@
+<?php
+//connection to csdm - webdev server and select DB
+$db = new mysqli(
+    'us-cdbr-azure-southcentral-f.cloudapp.net',
+    'b5051f6adfc120',
+    'c31439e7',
+    'webappalexander'
+);
+
+if($db->connect_errno){
+    die('Connection failed['.$db->connect_error.']');
+}
+
+$sql_query="SELECT * FROM marvelmovies WHERE UPPER (title) LIKE '%X-MEN%'";
+
+$result = $db->query($sql_query);
+
+echo "<h1> All X-Men Movies </h1>";
+
+while($row = $result->fetch_array()){
+    echo"<table>";
+    echo"<p>" . $row['title'] . "</p>";
+}
+
+$result->close();
+$db->close();
+
+?>
---
+++ MarvelAfter2010.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1302033
+ * Date: 10/10/2016
+ * Time: 15:55
+ */
