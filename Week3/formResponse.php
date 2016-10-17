72c3233ed670a6df4fae6620e42325bc29290320
Alexander Ladd <xander19@btinternet.com>
2016-10-17 16:18:14 +0100

    test

--- Week3/formResponse.php
+++ Week3/formResponse.php
@@ -7,10 +7,10 @@
 <body>
 <p>
     <?php
-    $forename = $_POST[“forename”];
-$surname = $_POST[“surname”];
-$gender = $_POST[“gender”];
-$mainsuperheropower = $_POST[“mainsuperheropower”];
+    $forename = $_POST['forename'];
+$surname = $_POST['surname'];
+$gender = $_POST['gender'];
+$mainsuperheropower = $_POST['mainsuperheropower'];
 echo $forename . $surname . $gender . $mainsuperheropower;
 
 ?>
--- Week3/superheroInput.php
+++ Week3/superheroInput.php
@@ -5,21 +5,8 @@
     <title>Superheroinput</title>
 </head>
 <body>
-<p>
-    <?php
-    //include the db
-    include ("Superheros_DB.php");
 
-    $forename = $_POST["forename"];
-    $surname = $_POST["surname"];
-    $gender = $_POST["gender"];
-    $mainsuperheropower = $_POST["mainsuperpower"];
-
-    $sql = "INSERT INTO superheroes (forename, surname, gender, mainsuperpower) VALUES ('$forename' , '$surname', '$gender', '$mainsuperheropower')"
-
-
-    ?>
-    <form action="formResponse.php" method="post">
+ <form action="formResponse.php" method="post">
         <label>Forename</Label><input type="text" name="forename">
         <label>Surname</Label><input type="text" name="surname">
         <label>Gender</Label><input type="text" name="gender">
@@ -27,6 +14,5 @@
         <input type="submit" value="Submit">
         </form>
 
-</p>
 </body>
 </html>
