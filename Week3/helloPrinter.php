dcbd8a1ed3bfcdf53a5269fb5326fa1b7c098a1a
Alexander Ladd <xander19@btinternet.com>
2016-10-17 13:41:15 +0100

    initial request

---
+++ Week3/helloPrinter.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Where would you like to say hello?</title>
+</head>
+<body>
+<p>
+    echo "testing";
+</p>
+</body>
+</html>
