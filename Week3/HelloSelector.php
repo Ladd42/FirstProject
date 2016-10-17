9d06d27adc336916a7f9181a556120c506865bcb
Alexander Ladd <xander19@btinternet.com>
2016-10-17 15:36:55 +0100

    test

--- Week3/HelloSelector.php
+++ Week3/HelloSelector.php
@@ -7,9 +7,9 @@
 <body>
 <p>
 
-    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=earth"><button>Earth<button></a>
-    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=mars"><button>Mars<button></a>
-    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=uranus"><button>Uranus<button></a>
+    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=earth"><button>Earth</button></a>
+    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=mars"><button>Mars</button></a>
+    <a href="http://webappalexander.azurewebsites.net/Week3/helloPrinter.php?planetname=uranus"><button>Uranus</button></a>
 
 
 </p>
