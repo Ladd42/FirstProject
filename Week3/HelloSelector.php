9483dc9bf70efbed8c02494e4e4c1291b9e9ce56
Alexander Ladd <xander19@btinternet.com>
2016-10-17 14:15:16 +0100

    initial request

--- Week3/HelloSelector.php
+++ Week3/HelloSelector.php
@@ -2,15 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Where would you like to say hello?</title>
+    <title>Where would you like to say hi?</title>
 </head>
 <body>
 <p>
-<form action=""helloPrinter.php" method ="post">
-    <a href="../Week3/helloPrinter.php"><button>Earth<button></a><br>
-    <a href="../Week3/helloPrinter.php"><button>Mars<button></a><br>
-    <a href="../Week3/helloPrinter.php"><button>Uranus<button></a><br>
-
+<form action="helloPrinter.php" method ="post">
+<input type="text" placeholder="Planet Name" name="planetname">
+<input type="sumbit" value='Go!'>
 </form>
 </p>
 </body>
