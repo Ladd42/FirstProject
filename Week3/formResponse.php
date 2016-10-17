cfddfd3769b3e0ef62cb0d4acca4be3b4cccc826
Alexander Ladd <xander19@btinternet.com>
2016-10-17 15:57:10 +0100

    test

--- Week3/formResponse.php
+++ Week3/formResponse.php
@@ -1,8 +1,19 @@
-<?php
-$forename = $_POST[“forename”];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>formResponse</title>
+</head>
+<body>
+<p>
+    <?php
+    $forename = $_POST[“forename”];
 $surname = $_POST[“surname”];
 $gender = $_POST[“gender”];
 $mainsuperheropower = $_POST[“mainsuperheropower”];
 echo $forename . $surname . $gender . $mainsuperheropower;
 
-?> 
+?>
+</p>
+</body>
+</html>
