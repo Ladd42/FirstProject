338889dee82269789729cd6f70cd2b1f59cb0ad3
Alexander Ladd <xander19@btinternet.com>
2016-10-17 15:55:10 +0100

    test

---
+++ Week3/formResponse.php
@@ -0,0 +1,8 @@
+<?php
+$forename = $_POST[“forename”];
+$surname = $_POST[“surname”];
+$gender = $_POST[“gender”];
+$mainsuperheropower = $_POST[“mainsuperheropower”];
+echo $forename . $surname . $gender . $mainsuperheropower;
+
+?> 
