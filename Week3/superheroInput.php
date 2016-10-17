9692b59e88651d52680e81d0c7f8f56f373dd0d7
Alexander Ladd <xander19@btinternet.com>
2016-10-17 15:48:34 +0100

    test

---
+++ Week3/superheroInput.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Alexander Test2</title>
+</head>
+<body>
+<p>
+    <?php
+    <form action="formResponse.php" method="post">
+        <label>Forename</Label><input type="text" name="forename">
+        <label>Surname</Label><input type="text" name="surname">
+        <label>Gender</Label><input type="text" name="gender">
+        <label>MainPower</Label><input type="text" name="mainsuperheropower">
+        <input type="submit" value="Submit">
+        </form>
+    ?>
+
+</p>
+</body>
+</html>
