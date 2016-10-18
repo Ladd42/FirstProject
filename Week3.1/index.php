0b7df37d9cd8e25818a54f95d7ee879bfee03996
Alexander <xander19@btinternet.com>
2016-10-18 13:44:50 +0100

    testing

--- Week3.1/index.php
+++ Week3.1/index.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 18/10/2016
- * Time: 13:32
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Alexander Test1</title>
+</head>
+<body>
+<header>
+    <h1> The Superhero System</h1>
+    <h2>Homepage</h2>
+</header>
+<p>What would you like to do?</p>
+<ui>
+    <li> <a href="insertSuperhero.php"</a></li>
+    <li> <a href="insertSuperhero.php"</a></li>
+    <li> <a href="insertSuperhero.php"</a></li>
+    <li> <a href="insertSuperhero.php"</a></li>
+</ui>
+</body>
+</html>
