25319dc9683770a317833a2d984bc78c39babd3f
Alexander <xander19@btinternet.com>
2016-10-18 14:11:09 +0100

    testing

--- Week3.1/index.php
+++ Week3.1/index.php
@@ -11,10 +11,25 @@
 </header>
 <p>What would you like to do?</p>
 <ui>
-    <li> <a href="insertSuperhero.php"</a></li>
-    <li> <a href="insertSuperhero.php"</a></li>
-    <li> <a href="insertSuperhero.php"</a></li>
-    <li> <a href="insertSuperhero.php"</a></li>
+    <li> <a href="insertSuperheros.php" Insert a superhero</a></li>
+    <li> <a href="displaySuperheros.php" Display all superheros</a></li>
+    <li> <a href="battle.php"Insert a battle  </a></li>
+    <li> <a href="displayBattles.php" Display all battles</a></li>
 </ui>
+<?
+include ("db_connect.php");
+$sql_query = "SELECT * FROM superheros";
+$result = $db->query($sql_query);
+while ($row = $result -> fetch_array())
+{
+    $firstname = $row['firstname'];
+    $lastname = $row['lastname'];
+    $id = $row['superheroID'];
+
+    echo "<li> <a href='displayBattles.php&id={$id}'>Battles for {$firstname} {$lastname}</a></li>";
+
+}
+?>
+
 </body>
 </html>
