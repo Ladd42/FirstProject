0343dd8feaa4ab173d47606a5a87947cbf4521c6
Alexander <xander19@btinternet.com>
2016-10-19 09:19:28 +0100

    testing

--- Week3.1/insertSuperheros.php
+++ Week3.1/insertSuperheros.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 18/10/2016
- * Time: 13:33
- */
+
+include ("db_connection.php");
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$superpower = $_POST["superpower"];
+
+$sql = "INSERT INTO  superheros (firstname, lastname, mainSuperpower) VALUES ('$firstname' , '$lastname' , '$superpower')";
+
+if ( mysqli_query ($db, $sql)) {
+
+} else {
+
+    echo "Error: " . $sql  . <br> .mysqli_error($db);
+
+}
+
+header("location:index.php");
+
+?>
