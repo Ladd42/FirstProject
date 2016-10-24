0ec831ec5448235845472e003c3831d100f2c37f
Alexander <xander19@btinternet.com>
2016-10-24 14:17:08 +0100

    testing

--- Week4/checklogin.php
+++ Week4/checklogin.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 24/10/2016
- * Time: 14:09
- */
+
+
+$username = $_POST["name"];
+$password = $_POST["password"];
+
+if ($username =="username" && $password=="password")
+{
+    setcookie('access_level_cookie', 'standarduser');
+
+}
+
+header('Location: loggedin.php');
+
+?>
