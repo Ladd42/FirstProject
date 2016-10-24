5e5827cd4d2a0073c7e3f0009339a61e8ea51ea4
Alexander <xander19@btinternet.com>
2016-10-24 14:30:08 +0100

    testing

--- Week4/loggedin.php
+++ Week4/loggedin.php
@@ -18,8 +18,8 @@ function displayAccessLevelInformation($accessLevel)
 
     }
     elseif ($accessLevel == "root") {
-     echo \"<p style = \\"background - color: red\\">You are currently logged in as a root User</p>";
-     echo \"<p style = \\"background - color: red\\">You have access to additional admin features</p>";
+     echo "<p<p style = \"background-color: red\">You are currently logged in as a root User</p>";
+     echo "<p<p style = \"background-color: red\">You have access to additional admin features</p>";
 
     }
 
