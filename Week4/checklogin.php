42b88762df327396d6fb889f01be7edadea7285c
Alexander <xander19@btinternet.com>
2016-10-24 17:10:56 +0100

    testing

--- Week4/checklogin.php
+++ Week4/checklogin.php
@@ -4,7 +4,7 @@
 $username = $_POST["name"];
 $password = $_POST["password"];
 
-if ($username =="username" && $password=="password")
+if ($username =="username" && $password =="password")
 {
     setcookie('access_level_cookie', 'StandardUser');
 }
