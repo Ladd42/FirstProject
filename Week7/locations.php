f57bd1340b8c4be37b0e51413475facf8dfdc0bd
Alexander <xander19@btinternet.com>
2016-11-07 14:36:09 +0000

    testing

--- Week7/locations.php
+++ Week7/locations.php
@@ -1,10 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 07/11/2016
- * Time: 14:22
- */
+<?PHP
+
+// connect to your Azure server and select database (remember you connection details are all on the azure portal
+$db = new mysqli(
+    "us-cdbr-azure-southcentral-f.cloudapp.net",
+    "b5051f6adfc120",
+    "c31439e7",
+    "webappalexander" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
 
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
