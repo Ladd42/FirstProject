1061e70cf0f0c1a91ed091d6df11b0cb1f966c69
Alexander <xander19@btinternet.com>
2016-11-29 23:59:44 +0000

    testing

--- Maps/route.php
+++ Maps/route.php
@@ -4,8 +4,10 @@
 </div>
 <!-- Replace the value of the key parameter with your own API key. -->
 <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAsaPQGyO2SHJumHMC2k8RTYfy3z7OXIk&callback=initMap">
+
 </script>
+<script type="text/javascript" src="http://webappalexander.azurewebsites.net/maps/routeplot.js"></script>
 
 <body>
 
