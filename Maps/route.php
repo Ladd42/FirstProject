19c68229d225ad32323a0e65cff510249d701b32
Alexander <xander19@btinternet.com>
2016-11-29 23:55:33 +0000

    testing

--- Maps/route.php
+++ Maps/route.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 29/11/2016
- * Time: 23:49
- */
+<div id="map"></div>
+<div id="right-panel">
+    <p>Total Distance: <span id="total"></span></p>
+</div>
+<!-- Replace the value of the key parameter with your own API key. -->
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">
+</script>
+
+<body>
+
+</body>
