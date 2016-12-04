55e942b63030c69c33b18a472ec84d4fa3f9e951
Alexander <xander19@btinternet.com>
2016-12-04 18:01:59 +0000

    testing

--- Maps/routeplot.js
+++ Maps/routeplot.js
@@ -11,9 +11,12 @@ function initMap() {
         panel: document.getElementById('right-panel')
     });
 
+    directionsDisplay.addListener('directions_changed', function() {
+        computeTotalDistance(directionsDisplay.getDirections());
+    });
 
     displayRoute('Aberdeen, Scotland', 'Portlethen, Scotland', directionsService,
-        directionsDisplay);
+        );
 }
 
 function displayRoute(origin, destination, service, display) {
@@ -23,5 +26,21 @@ function displayRoute(origin, destination, service, display) {
         waypoints: [{location: 'Torry, Scotland'}, {location: 'Cove Bay, Scotland'}],
         travelMode: 'WALKING',
         avoidTolls: true
-    },
+    }, function(response, status) {
+        if (status === 'OK') {
+            display.setDirections(response);
+        } else {
+            alert('Could not display directions due to: ' + status);
+        }
+    });
+}
+
+function computeTotalDistance(result) {
+    var total = 0;
+    var myroute = result.routes[0];
+    for (var i = 0; i < myroute.legs.length; i++) {
+        total += myroute.legs[i].distance.value;
+    }
+    total = total / 1000;
+    document.getElementById('total').innerHTML = total + ' km';
 }
