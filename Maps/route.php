47a7c885720cc8065662439c2c8f2b7dddefd9a1
Alexander <xander19@btinternet.com>
2016-12-04 17:53:22 +0000

    testing

--- Maps/route.php
+++ Maps/route.php
@@ -3,9 +3,6 @@
     <script type="text/javascript" src="http://webappalexander.azurewebsites.net/maps/routeplot.js"></script>
 </head>
 <div id="map"></div>
-<div id="right-panel">
-    <p>Total Distance: <span id="total"></span></p>
-</div>
 <!-- Replace the value of the key parameter with your own API key. -->
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAsaPQGyO2SHJumHMC2k8RTYfy3z7OXIk&callback=initMap">
